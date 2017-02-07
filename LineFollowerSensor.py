bf30af7a23d92a2981d96bd558e55c4aad1581c9
Matt <matt_lees@yahoo.com>
2017-02-07 22:30:34 +0000

    Fixed issues with the LineFollerSensor

--- LineFollowerSensor.py
+++ LineFollowerSensor.py
@@ -10,15 +10,13 @@ class LineFollowerSensor:
     """
     """
     
-    # Define GPIO to use on Pi
-    GPIO_LINE_L = 16
-    GPIO_LINE_M = 21
-    GPIO_LINE_R = 20
-    
-    def __init__(self):
+    def __init__(self, left_sensor_id, middle_sensor_id, right_sensor_id):
         """
         """
         module_logger.info("Setting up LineFollowerSensor Module")
+        self.left_sensor_id = left_sensor_id
+        self.middle_sensor_id = middle_sensor_id
+        self.right_sensor_id = right_sensor_id
         
         # Use BCM GPIO references
         # instead of physical pin numbers
@@ -26,29 +24,34 @@ class LineFollowerSensor:
         GPIO.setwarnings(False)
 
         # Set pins as output and input
-        GPIO.setup(self.GPIO_LINE_L, GPIO.IN)       # Switch as input
-        GPIO.setup(self.GPIO_LINE_M, GPIO.IN)       # Switch as input
-        GPIO.setup(self.GPIO_LINE_R, GPIO.IN)       # Switch as input
+        GPIO.setup(self.left_sensor_id, GPIO.IN)       # Switch as input
+        GPIO.setup(self.middle_sensor_id, GPIO.IN)       # Switch as input
+        GPIO.setup(self.right_sensor_id, GPIO.IN)       # Switch as input
 
     def GetLState(self):
         """
         """
-        return GPIO.input(self.GPIO_LINE_L)
+        return GPIO.input(self.left_sensor_id)
 
     def GetMState(self):
         """
         """
-        return GPIO.input(self.GPIO_LINE_M)
+        return GPIO.input(self.middle_sensor_id)
 
     def GetRState(self):
         """
         """
-        return GPIO.input(self.GPIO_LINE_R)
+        return GPIO.input(self.right_sensor_id)
 
     
 if __name__ == "__main__":
     try:
-        LineFollowerSensor linefollower
+        # Define GPIO to use on Pi
+        GPIO_LINE_L = 16
+        GPIO_LINE_M = 21
+        GPIO_LINE_R = 20
+        
+        linefollower = LineFollowerSensor(GPIO_LINE_L, GPIO_LINE_M, GPIO_LINE_R)
         print("linefollower::left: ", linefollower.GetLState())
         print("linefollower::right: ", linefollower.GetRState())
         print("linefollower::middle: ", linefollower.GetMState())
