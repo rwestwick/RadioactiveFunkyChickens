a001570681a725df38d066ff8a40ddb15c5a6118
Matt <matt_lees@yahoo.com>
2017-02-07 22:26:24 +0000

    Fixed issues with the IRSensor

--- IRSensor.py
+++ IRSensor.py
@@ -10,13 +10,14 @@ module_logger = logging.getLogger("__main__.IRSensor")
 #irFR = 11
 #lineLeft = 29
 #lineRight = 13
-    
+
 class IRSensor:
     """
     """
 
     def __init__(self, id):
         """
+        Initialise the parameters required for the IR Sensor
         """
         self.gpio_id = id
         module_logger.info("Setting up IRSensor Module")
@@ -25,28 +26,22 @@ class IRSensor:
         GPIO.setmode(GPIO.BOARD)
 
         #set up digital line detectors as inputs
-        GPIO.setup(lineRight, GPIO.IN) 
+        GPIO.setup(self.gpio_id, GPIO.IN) 
 
-    #======================================================================
-    # IR Sensor Functions
-    #
-    # irLeft(): Returns state of Left IR Obstacle sensor
-    def irActive():
+    def irActive(self):
         """
+        Returns state of Left IR Obstacle sensor
         """
         if GPIO.input(self.gpio_id) == 0:
             return True
         else:
             return False
         
-    # End of IR Sensor Functions
-    #======================================================================
-
     
 if __name__ == "__main__":
     try:
-        IRSensor sensor(7)
-        print("irActive: ", irActive())
+        sensor = IRSensor(7)
+        print("irActive: ", sensor.irActive())
     except KeyboardInterrupt:
         pass
     finally:
