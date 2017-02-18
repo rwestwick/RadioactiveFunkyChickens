720a220b058c36cfe0f15dcca88ee4aab9dcf6bd
Matt <matt@mlees.net>
2017-02-18 22:47:30 +0000

    Updates to scripts and line endings

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,2 +1,32 @@
+#!/bin/bash
+
+rm -f *.xml
+rm -f *.sc
+rm -f pylint.txt
+rm -f flake8.txt
+
+
+#--------------------------------------------------------------
+# Run the pep8 utilities 
+#--------------------------------------------------------------
+echo "**************** $(date) - Running pylint ****************"
 find . -name \*.py | xargs pylint > pylint.txt
+echo "**************** $(date) - pylint complete ****************"
+
+
+echo "**************** $(date) - Running flake8 ****************"
 find . -name \*.py | xargs flake8 > flake8.txt
+echo "**************** $(date) - flake8 complete ****************"
+
+
+#--------------------------------------------------------------
+# Run the sloccount utility
+#--------------------------------------------------------------
+echo "**************** $(date) - Running cloc ****************"
+
+cloc --by-file \
+     --xml \
+     --out=./cloc.xml \
+     --exclude-dir=.git,.svn \
+
+echo "**************** $(date) - cloc complete ****************"
