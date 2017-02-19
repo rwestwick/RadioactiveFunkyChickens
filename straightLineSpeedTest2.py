b72b9bf6dcb08bb4b93bc199778f24dd6c97535f
Matt <matt_lees@yahoo.com>
2017-02-19 01:01:55 +0000

    Modified straight line speed test to use the new sensor and controller modules

--- straightLineSpeedTest2.py
+++ straightLineSpeedTest2.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
-#
-# straightLineSpeedTest2.py needs to be run from the command line
-#
-# This code is for use in the Straight Line Speed Test - PiWars 2017 challenge
-# http://piwars.org/
-#
-#======================================================================
+
+"""
+straightLineSpeedTest2.py needs to be run from the command line
+This code is for use in the Straight Line Speed Test - PiWars 2017 challenge
+http://piwars.org/
+"""
 
 # Import required libraries
-import sonarRFC
 import time
+import logging
+import math # https://docs.python.org/2/library/math.html
+import KeyboardCharacterReader
+import MotorController
+import UltrasonicSensor
+
+# Create a logger to both file and stdout
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)25s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 # Set initial constant values
 
@@ -21,153 +33,111 @@ firstBufferWidth = 20     # First steer correction distance to nearest wall
 secondBufferWidth = 10    # Second steer correction distance to nearest wall
 loopTime = 2.0            # Correction loop speed in seconds. This could be zero!
 
-##########################
-# Define local functions #
-##########################
-
-#======================================================================
-# Reading single character by forcing stdin to raw mode
-import sys
-import tty
-import termios
-
-def readchar():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    if ch == '0x03':
-        raise KeyboardInterrupt
-    return ch
-
-def readkey(getchar_fn=None):
-    getchar = getchar_fn or readchar
-    c1 = getchar()
-    if ord(c1) != 0x1b:
-        return c1
-    c2 = getchar()
-    if ord(c2) != 0x5b:
-        return c1
-    c3 = getchar()
-    return chr(0x10 + ord(c3) - 65)  # 16=Up, 17=Down, 18=Right, 19=Left arrows
-
-# End of single character reading
-#======================================================================
-
-#======================================================================
-# Calaculate angle to wall
-import math # https://docs.python.org/2/library/math.html
-
-# wallAngle(distanceOne, distanceTwo): returns angle from front to rear line of robot to
-#                                      the wall in degrees (90deg means robot is driving
-#                                      parallel to the walls)
-#                                      N.B. Angle does not tell if robot is pointing to or away
-#                                      from wall.
-#                                      inputs:
-#                                       distanceOne - Ultrasonic measurement from one side of robot
-#                                       distanceTwo - Ultrasonic measurement from other side of robot
-# N.B. Calculation is based on inputs both being in cm and walls being wallWidth apart.
 
 def wallAngle(distanceOne, distanceTwo):
+    """
+    Calculate angle to wall
+    returns angle from front to rear line of robot to
+    the wall in degrees (90deg means robot is driving
+    parallel to the walls)
+    N.B. Angle does not tell if robot is pointing to or away
+    from wall.
+    inputs:
+    distanceOne - Ultrasonic measurement from one side of robot
+    distanceTwo - Ultrasonic measurement from other side of robot
+    N.B. Calculation is based on inputs both being in cm and walls being wallWidth apart.
+    """
     yOne = distanceOne + (robotWidth/2)
     yTwo = distanceTwo + (robotWidth/2)
-
     hypotenuse = yOne + yTwo
-
     theta = math.degrees(math.asin(wallWidth/hypotenuse))
-    
     return theta
 
-# distanceFromWall(distanceOne, theta): returns distance from front to rear line of robot to
-#                                       the wall that is at theta degrees angle to wall at a
-#                                       distance of distance distanceOne.
-#                                       Unit of returned value is the same as input e.g. cm
 
 def distanceFromWall(distanceOne, theta):
+    """
+    returns distance from front to rear line of robot to
+    the wall that is at theta degrees angle to wall at a
+    distance of distance distanceOne.
+    Unit of returned value is the same as input e.g. cm
+    """
     hypotenuse = distanceOne + (robotWidth/2)
-
     wallDistance = hypotenuse * math.sin(math.radians(theta))
-
     return wallDistance
 
-# End of angle to wall calculations
-#======================================================================
-
-##########################
-# Main program           #
-##########################
-
-# Initialise motors
-sonarRFC.initMotors()
 
 # Create necessary sensor objects
-viewLeft = sonarRFC.leftUltraSensor()
-viewRight = sonarRFC.rightUltraSensor()
+SONAR_INPUT_RIGHT = 16  # Connected to Echo
+SONAR_OUTPUT_RIGHT = 31  # Connected to Trig
+SONAR_INPUT_LEFT = 15  # Connected to Echo
+SONAR_OUTPUT_LEFT = 12  # Connected to Trig
+viewLeft = UltrasonicSensor.UltrasonicSensor(SONAR_INPUT_LEFT, SONAR_OUTPUT_LEFT)
+viewRight = UltrasonicSensor.UltrasonicSensor(SONAR_INPUT_RIGHT, SONAR_OUTPUT_RIGHT)
 
 time.sleep(1)
 
 # Sanity check the distance from edge of robot to walls
-initialLeftDistance = viewLeft.Measurement()
-initialRightDistance = viewRight.Measurement()
+initialLeftDistance = viewLeft.measurement()
+initialRightDistance = viewRight.measurement()
 
 if ((initialLeftDistance + initialRightDistance + robotWidth) > (wallWidth * 1.2)):
-    print("The walls are too far apart!")
+    logger.warn("The walls are too far apart!")
 else:
-    print("The walls are not too far apart!")
+    logger.info("The walls are not too far apart!")
 
 # Waiting for start of race
-print("To start race press 'Space' key.")
+logger.info("To start race press 'Space' key.")
 
 while True:
-        keyp = readkey()
+        keyp = KeyboardCharacterReader.readkey()
         if keyp == ' ':
-            print("Go")
+            logger.info("Go")
             break
 
-# Drive forward at full speed
-sonarRFC.forward(speed)
+# Initialise motors
+robotmove = MotorController.MotorController()
 
-print("Their off! Press Control^C to finish")
+# Drive forward at full speed
+robotmove.forward(speed)
+
+logger.info("They're off! Press Control^C to finish")
 
 # Try to avoid the walls!
 try:
     while True:
         # Calculate the distance from edge of robot to walls
-        leftDistance = viewLeft.Measurement()
-        print("Average measured left distance: " + str(int(leftDistance)) + " cm")
-        rightDistance = viewRight.Measurement()
-        print("Average measured right distance: " + str(int(rightDistance)) + " cm")
+        leftDistance = viewLeft.measurement()
+        logger.info("Average measured left distance: " + str(int(leftDistance)) + " cm")
+        rightDistance = viewRight.measurement()
+        logger.info("Average measured right distance: " + str(int(rightDistance)) + " cm")
         angleFromWall = wallAngle(leftDistance, rightDistance)
-        print("Angle from the wall: " + str(int(angleFromWall)) + " deg")
+        logger.info("Angle from the wall: " + str(int(angleFromWall)) + " deg")
         spaceFromLeftWall = distanceFromWall(leftDistance, angleFromWall)
-        print("Actual distance from the left wall: " + str(int(spaceFromLeftWall)) + " cm")
+        logger.info("Actual distance from the left wall: " + str(int(spaceFromLeftWall)) + " cm")
 
         # Decide which way to steer
         if (leftDistance < firstBufferWidth):
-            sonarRFC.turnForward(speed, (speed - arcSpeed))
-            print("Steering right")
+            robotmove.turn_forward(speed, (speed - arcSpeed))
+            logger.info("Steering right")
         elif (rightDistance < firstBufferWidth):
-            sonarRFC.turnForward((speed - arcSpeed), speed)
-            print("Steering left")
+            robotmove.turn_forward((speed - arcSpeed), speed)
+            logger.info("Steering left")
         elif ((rightDistance < secondBufferWidth) and (leftDistance < secondBufferWidth)):
-            sonarRFC.stop()
-            print("The walls are caving in!")
+            robotmove.stop()
+            logger.info("The walls are caving in!")
         elif ((rightDistance > wallWidth) and (leftDistance > wallWidth)):
-            sonarRFC.stop()
-            print("I'm in the clear!")
+            robotmove.stop()
+            logger.info("I'm in the clear!")
         else:
-            sonarRFC.forward(speed)
-            print("Storming forward!")
+            robotmove.forward(speed)
+            logger.info("Storming forward!")
         
         # Loop delay
         time.sleep(loopTime)
         
 except KeyboardInterrupt:
-    print("Stopping the race")
+    logger.info("Stopping the race")
 
 # Sets all motors off and sets GPIO to standard values
 finally:
-    sonarRFC.cleanup()
+    robotmove.cleanup()
