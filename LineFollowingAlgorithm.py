4853251a5d773f7a33d7f1bc89a99c6ed06dc18e
Matt <matt_lees@yahoo.com>
2017-02-19 16:04:33 +0000

    Updated Line following algo

--- LineFollowingAlgorithm.py
+++ LineFollowingAlgorithm.py
@@ -9,6 +9,8 @@ import MotorController
 import LineFollowerSensor
 import SetupConsoleLogger
 import GPIOLayout
+import KeyboardCharacterReader
+
 
 logger = logging.getLogger(__name__)
 SetupConsoleLogger.setup_console_logger(logger)
@@ -22,6 +24,16 @@ robotmove = MotorController.MotorController(GPIOLayout.MOTOR_LEFT_FORWARD_PIN,
 
 def main():
     logger.info("Line Follower With Move")
+
+    # Waiting for start of race
+    logger.info("To start line following press 'Space' key.")
+
+    while True:
+        keyp = KeyboardCharacterReader.readkey()
+        if keyp == ' ':
+            logger.info("Go")
+            break
+
     foundline = False
     lastturnleft = False
     lastturnright = False
@@ -36,7 +48,7 @@ def main():
         RSTATE = linefollower.get_r_state()
 
         if foundline == False:
-            robotmove.forward(robotmove.SPEED_SLOW)
+            robotmove.forward(MotorController.SPEED_SLOW)
             if LSTATE == 1 or MSTATE == 1 or RSTATE == 1:
                 foundline = True
         else:
@@ -44,13 +56,13 @@ def main():
                 logger.info("No Line")
                 if lastturnleft:
                     logger.info("  Hard Left Search")
-                    robotmove.spin_left(robotmove.SPEED_VERYVERYSLOW)
+                    robotmove.spin_left(MotorController.SPEED_VERYVERYSLOW)
                 elif lastturnright:
                     logger.info("  Hard Right Search")
-                    robotmove.spin_right(robotmove.SPEED_VERYVERYSLOW)
+                    robotmove.spin_right(MotorController.SPEED_VERYVERYSLOW)
                 else:
                     logger.info("  Search Right")
-                    robotmove.spin_right(robotmove.SPEED_VERYVERYSLOW)
+                    robotmove.spin_right(MotorController.SPEED_VERYVERYSLOW)
 
             elif LSTATE == 0 and MSTATE == 1 and RSTATE == 0:
                 logger.info("Found Middle")
@@ -65,41 +77,41 @@ def main():
                     robotmove.TurnBackLeft()
                 """
                 logger.info("  Forward")
-                robotmove.forward(robotmove.SPEED_FAST)
+                robotmove.forward(MotorController.SPEED_FAST)
                 lastturnleft = False
                 lastturnright = False
             elif (LSTATE == 1 and MSTATE == 0 and RSTATE == 0):
                 logger.info("Left only - Turn Hard Left")
-                robotmove.spin_left(robotmove.SPEED_SLOW)
+                robotmove.spin_left(MotorController.SPEED_SLOW)
                 lastturnleft = True
                 lastturnright = False
             elif (LSTATE == 1 and MSTATE == 1 and RSTATE == 0):
                 logger.info("Left and middle - Turn Left")
-                robotmove.one_wheel_left(robotmove.SPEED_SLOW)
+                robotmove.one_wheel_left(MotorController.SPEED_SLOW)
                 lastturnleft = True
                 lastturnright = False
             elif (LSTATE == 0 and MSTATE == 0 and RSTATE == 1):
                 logger.info("Right only - Turn Hard Right")
-                robotmove.spin_right(robotmove.SPEED_SLOW)
+                robotmove.spin_right(MotorController.SPEED_SLOW)
                 lastturnleft = False
                 lastturnright = True
             elif (LSTATE == 0 and MSTATE == 1 and RSTATE == 1):
                 logger.info("Right and middle - Turn Right")
-                robotmove.one_wheel_right(robotmove.SPEED_SLOW)
+                robotmove.one_wheel_right(MotorController.SPEED_SLOW)
                 lastturnleft = False
                 lastturnright = True
             elif LSTATE == 1 and MSTATE == 1 and RSTATE == 1:
                 logger.info("All sensors are on the line")
-                robotmove.forward(robotmove.SPEED_MEDIUM)
+                robotmove.forward(MotorController.SPEED_MEDIUM)
             else:
                 logger.error("Some other state")
-                robotmove.reverse(robotmove.SPEED_MEDIUM)
+                robotmove.reverse(MotorController.SPEED_MEDIUM)
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        pass
+        logger.info("Stopping the race")
     finally:
         logger.info("Line Follower Finished")
         robotmove.cleanup()
