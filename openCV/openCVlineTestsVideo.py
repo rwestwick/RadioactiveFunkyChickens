c8450e57078cd1bd2c1454b4eca9e772284b6684
rwestwick <robert.westwick@gmail.com>
2017-03-05 18:04:08 +0000

    Added comments to clarify what each of the function parameters are.

--- openCV/openCVlineTestsVideo.py
+++ openCV/openCVlineTestsVideo.py
@@ -1,4 +1,8 @@
 # Run from command line allows the window to close when 'q' is pressed
+
+# https://www.python.org/dev/peps/pep-0238/
+# The future division statement, spelled "from __future__ import division", will change the / operator to mean true division
+# throughout the module.
 from __future__ import division
 
 # Import OpenCV and NumPy
@@ -16,16 +20,22 @@ camera = PiCamera()
 camera.resolution = (CAMERA_WIDTH, CAMERA_HEIGHT)
 camera.framerate = 10
 camera.vflip = True
+
+# http://picamera.readthedocs.io/en/release-1.10/api_array.html
+# class picamera.array.PiRGBArray(camera, size=None)[source]
+# Produces a 3-dimensional RGB array from an RGB capture.
 rawCapture = PiRGBArray(camera, size=(CAMERA_WIDTH, CAMERA_HEIGHT))
 
-# allow the camera to warmup
+# Allow the camera time to warmup
 time.sleep(0.1)
 
-# row values
-rowValues = np.zeros(ROW_LENGTH)
+# Initialize rowValues array to do testing such that they are all initialised to be white
+rowValues = np.ones(ROW_LENGTH) * 255
 
 # capture frames from the camera
-# See picamera.readthedocs.io/en/release-1.10
+# http://picamera.readthedocs.io/en/release-1.10/api_camera.html
+# capture_continuous(output, format=None, use_video_port=False, resize=None, splitter_port=0, burst=False, **options)
+# The format, use_video_port, splitter_port, resize, and options parameters are the same as in capture()
 for frame in camera.capture_continuous(rawCapture, format="rgb", use_video_port=True):
     # grab the raw NumPy array respresenting the image, then intialize the timestap
     # and occupied/unoccupied text
@@ -35,6 +45,17 @@ for frame in camera.capture_continuous(rawCapture, format="rgb", use_video_port=
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
     # Simple thresholding
+    # http://www.pyimagesearch.com/2014/09/08/thresholding-simple-image-segmentation-using-opencv/
+    # (T, threshImage) = cv2.threshold(src, thresh, maxval, type)
+    # src - source image. This image should be grayscale.
+    # thresh - is the threshold value which is used to classify the pixel intensities in the grayscale image.
+    # maxval - is the pixel value used if any given pixel in the image passes the thresh  test.
+    # type - the thresholding method to be used. The type  value can be any of:
+    # cv2.THRESH_BINARY
+    # cv2.THRESH_BINARY_INV
+    # cv2.THRESH_TRUNC
+    # cv2.THRESH_TOZERO
+    # cv2.THRESH_TOZERO_INV
     ret,threshImg = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)
 
 ##    print("Size of threshImg array: ", threshImg.shape)
@@ -72,7 +93,8 @@ for frame in camera.capture_continuous(rawCapture, format="rgb", use_video_port=
         print rowValues
         print smallSquare
 
-    # clear the stream in preperation for the next frame
+    # http://picamera.readthedocs.io/en/release-1.10/api_array.html
+    # Clear the stream in preperation for the next frame
     rawCapture.truncate(0)
 
     # if the 'q' key was pressed break from the loop
