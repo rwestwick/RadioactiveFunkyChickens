e98210a5fb7a107cf88b315de216aa86a4718296
Matt <matt_lees@yahoo.com>
2017-03-25 17:48:39 +0000

    further refinements on the minimal maze

--- UltrasonicSensor.py
+++ UltrasonicSensor.py
@@ -21,14 +21,15 @@ class UltrasonicSensor(object):  # pylint: disable=too-few-public-methods
     """
     Class defines how to interract with the Ultrasonic sensor
     """
-    # Running average settings
-    QSIZE = 3
-    QINITIAL = 30.0  # Based on wall to wall distance of ~60cm
 
     def __init__(self, input_pin, output_pin=None):
         """
         Initialises the class
         """
+
+        # Running average settings
+        self.QSIZE = 1
+
         GPIO.setwarnings(False)
 
         self.sonar_in = input_pin
@@ -58,13 +59,12 @@ class UltrasonicSensor(object):  # pylint: disable=too-few-public-methods
         # Initilise Queue
         self.queue = Queue.Queue()
 
-        #for _ in xrange(0, self.QSIZE):
-        #    self.queue.put(self.QINITIAL)
 
     def measurement(self):
         """
         Returns the distance in cm to the nearest reflecting object
         """
+        self.queue = Queue.Queue()
 
         for x in range(0, self.QSIZE):
             # If the two pins are actually the same, then
@@ -72,6 +72,12 @@ class UltrasonicSensor(object):  # pylint: disable=too-few-public-methods
             # output
             if self.sonar_out == self.sonar_in:
                 GPIO.setup(self.sonar_out, GPIO.OUT)
+
+            # Set trigger to False (Low)
+            GPIO.output(self.sonar_out, False)
+
+            # Allow module to settle
+            time.sleep(0.1)
 
             # Send 10us pulse to trigger
             GPIO.output(self.sonar_out, True)
@@ -108,8 +114,13 @@ class UltrasonicSensor(object):  # pylint: disable=too-few-public-methods
 
         # Calculate running average
         total = 0
+        stringvals = ""
         for val in self.queue.queue:
+            stringvals += format(val, '.2f') + " : "
             total = total + val
+
+        #stringvals += "avg " + format((total / self.queue.qsize()), '.2f')
+        MODULE_LOGGER.info("Ultrasonic values: " + stringvals)
 
         return total / self.queue.qsize()
 
