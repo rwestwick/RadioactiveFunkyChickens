6f00310592f8281e16f04957e4d6d64318c2d89e
Rob Westwick <robert.westwick@gmail.com>
2017-04-06 22:00:52 +0100

    Needed to add extra comment line.

---
+++ openCV/openCVpythonVM.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# The Bash script that needs to run to bring
+# up the Python VM to run OpenCV for the installation
+# from www.pyimagesearch.com
+# Need to run as source openCVpython.sh for VM to open in
+# calling shell
+
+source ~/.profile
+workon cv
