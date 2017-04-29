44b41832d7af7e3e3a069411965a1bdbcbae19e4
Rob Westwick <robert.westwick@gmail.com>
2017-04-29 18:25:32 +0100

    Made changes from pep8

--- WiimoteControllerNunchuk.py
+++ WiimoteControllerNunchuk.py
@@ -21,7 +21,7 @@ import SetupConsoleLogger
 import GPIOLayout
 import cwiid
 
-STICK_DELAY = 0.1 
+STICK_DELAY = 0.1
 
 # Create a logger to both file and stdout
 LOGGER = logging.getLogger(__name__)
@@ -33,33 +33,34 @@ robotmove = MotorController.MotorController(
     GPIOLayout.MOTOR_LEFT_BACKWARD_PIN,
     GPIOLayout.MOTOR_RIGHT_FORWARD_PIN,
     GPIOLayout.MOTOR_RIGHT_BACKWARD_PIN)
-    
+
+
 def main():
     """
     """
     # Set initial values
     speed = MotorController.SPEED_FASTEST  # Initial forward speed
-    
+
     # Connecting to the wiimote. This allows several attempts
     # as first few often fail.
     LOGGER.info("Press 1+2 on your Wiimote now ...")
     time.sleep(1)
 
     wm = None
-    i=2
+    i = 2
     while not wm:
             try:
-                    wm=cwiid.Wiimote()
+                    wm = cwiid.Wiimote()
             except RuntimeError:
-                    if (i>5):
+                    if (i > 5):
                             LOGGER.info("Cannot create Wiimote connection.")
                             quit()
-                    LOGGER.info("Error opening wiimote connection, attempt "
-                                + str(i))
-                    i +=1
+                    LOGGER.info("Error opening wiimote connection, attempt " +
+                                str(i))
+                    i += 1
 
     LOGGER.info("Wiimote connected.")
-    
+
     # Set wiimote to report button presses
     wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_EXT
 
@@ -68,10 +69,10 @@ def main():
 
     # Respond to Nunchuk joystick
     while True:
-        
+
         if wm.state.has_key('nunchuk'):
             # print("Success")
-            
+
             # X axis: Left Max = 25, Middle = 125, RightMax = 225
             NunchukStickX = (wm.state['nunchuk']['stick'][cwiid.X])
             # Y axis: DownMax = 30, Middle = 125, UpMax = 225
@@ -92,14 +93,14 @@ def main():
                 robotmove.forward(speed)
                 LOGGER.info("Forward at speed " + str(speed))
                 time.sleep(STICK_DELAY)
-            
+
             # Go backwards if joystick pulled back
             elif (NunchukStickY < 100) & (NunchukStickY > 50):
                 speed = MotorController.SPEED_SLOW
                 robotmove.reverse(speed)
                 LOGGER.info("Reverse at speed " + str(speed))
                 time.sleep(STICK_DELAY)
-                
+
             elif (NunchukStickY <= 50):
                 speed = MotorController.SPEED_FASTEST
                 robotmove.reverse(speed)
@@ -118,7 +119,7 @@ def main():
                 robotmove.spin_right(speed)
                 LOGGER.info("Spin right at speed " + str(speed))
                 time.sleep(STICK_DELAY)
-            
+
             # Spin left if joystick pushed left
             elif (NunchukStickX < 100) & (NunchukStickX > 50):
                 speed = MotorController.SPEED_SLOW
@@ -136,7 +137,7 @@ def main():
             else:
                 robotmove.stop()
                 LOGGER.info("Stop!")
-            
+
         else:
             print("Doh for now")
             time.sleep(2)
