1656857f52dcd99acb6f7bcce66fbff56fa50342
Matt <matt@mlees.net>
2017-11-26 22:53:22 +0000

    Added Dual Motor driver controller Fixed gpio to match the correct wheels Updated dual motor tester programme

--- TestDriverTwoControllersAlgorithm.py
+++ TestDriverTwoControllersAlgorithm.py
@@ -31,7 +31,13 @@ REARDRIVE = MotorController.MotorController(
     GPIOLayout.MOTOR_RIGHT_REAR_FORWARD_PIN,
     GPIOLayout.MOTOR_RIGHT_REAR_BACKWARD_PIN)
 
-    
+
+def one_wheel():
+    """
+    Performs single wheel movements
+    """
+
+
 def main():
     """
     Performs the main driving algo test
@@ -40,16 +46,55 @@ def main():
     REARDRIVE.stop()
     FRONTDRIVE.stop()
     REARDRIVE.stop()
-    
-    FRONTDRIVE.forward(SPEED_MEDIUM)
-    REARDRIVE.forward(SPEED_MEDIUM)
-    time.sleep(2)
-    
-    FRONTDRIVE.spin_right(SPEED_MEDIUM)
-    REARDRIVE.spin_right(SPEED_MEDIUM)
-    time.sleep(2)
+  
+    speed_list = [MotorController.SPEED_FASTEST,
+                  MotorController.SPEED_MEDIUM,
+                  MotorController.SPEED_VERYVERYSLOW]
 
-    
+    for x in speed_list:
+		LOGGER.info("Speed " + str(x))
+		FRONTDRIVE.stop()
+		REARDRIVE.stop()
+
+		LOGGER.info("forward")
+		FRONTDRIVE.forward(x)
+		REARDRIVE.forward(x)
+		time.sleep(5)
+
+		LOGGER.info("reverse")
+		FRONTDRIVE.reverse(x)
+		REARDRIVE.reverse(x)
+		time.sleep(3)
+
+		LOGGER.info("spin_right")
+		FRONTDRIVE.spin_right(x)
+		REARDRIVE.spin_right(x)
+		time.sleep(5)
+
+		LOGGER.info("spin_left")
+		FRONTDRIVE.spin_left(x)
+		REARDRIVE.spin_left(x)
+		time.sleep(5)
+
+		LOGGER.info("turn_forward r")
+		FRONTDRIVE.turn_forward(x, 0)
+		REARDRIVE.turn_forward(x, 0)
+		time.sleep(5)
+
+		LOGGER.info("turn_forward l")
+		FRONTDRIVE.turn_forward(0, x)
+		REARDRIVE.turn_forward(0, x)
+		time.sleep(5)
+
+		LOGGER.info("turn_reverse r")
+		FRONTDRIVE.turn_reverse(x, 0)
+		REARDRIVE.turn_reverse(x, 0)
+		time.sleep(5)
+
+		LOGGER.info("turn_reverse l")
+		FRONTDRIVE.turn_reverse(0, x)
+		REARDRIVE.turn_reverse(0, x)
+		time.sleep(5)
 
     
 if __name__ == "__main__":
