b6a0d23e34288410f018e6135c827e36a22f7620
Matt <matt@mlees.net>
2017-12-26 03:38:02 +0000

    added missing file

---
+++ GPIOZeroStub.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+"""
+Provides a test version of the GPIOZero.GPIODevice class for platforms where
+it doesn't exist.
+"""
+
+
+class GPIODevice:
+    """
+    Provides a stub for the GPIODevice class defined in GPIOZero
+    """
+
+    def __init__(self, param1):
+        """
+        Stub function
+        """
+
+    def on(self):
+        """
+        Stub function
+        """
+
+    def off(self):
+        """
+        Stub function
+        """
+
+    def close(self):
+        """
+        Stub function
+        """
