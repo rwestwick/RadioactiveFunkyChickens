e2bf7b7a3b390de6e8ce308bcd8f82d7d0f96178
Matt <matt@mlees.net>
2017-12-26 21:04:07 +0000

    Fixed some warnings

--- GPIOStub.py
+++ GPIOStub.py
@@ -4,14 +4,14 @@ Provides a test version of the GPIO class for platforms where
 it doesn't exist.
 """
 
-from random import *
+import random
 
 BOARD = 1
 OUT = 2
 IN = 3
 
 
-class pwmObject:
+class pwmObject(object):
     """
     Provides a stub for the PWM class defined in Rpi
     """
@@ -25,48 +25,56 @@ class pwmObject:
         """
         Stub function
         """
+        del param
 
     def ChangeDutyCycle(self, param1):
         """
         Stub function
         """
+        del param1
 
 
 def setmode(param):
     """
     Stub function
     """
+    del param
 
 
 def setwarnings(param):
     """
     Stub function
     """
+    del param
 
 
 def setup(param1, param2):
     """
     Stub function
     """
+    del param2
 
 
 def output(param1, param2):
     """
     Stub function
     """
+    del param2
 
 
 def input(param1):
     """
     Stub function
     """
-    return randint(0, 1)
+    del param1
+    return random.randint(0, 1)
 
 
 def PWM(param1, param2):
     """
     Stub function
     """
+    del param1, param2
     return pwmObject()
 
 
