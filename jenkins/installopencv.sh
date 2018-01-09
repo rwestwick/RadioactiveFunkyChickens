6d03f4fd68998bde42d4a7201aef0e193e130f95
Matt Lees <matt_lees@yahoo.com>
2018-01-09 22:51:39 +0000

    updated install scripts following painful reinstall

--- jenkins/installopencv.sh
+++ jenkins/installopencv.sh
@@ -2,15 +2,6 @@
 
 # https://www.pyimagesearch.com/2017/09/04/raspbian-stretch-install-opencv-3-python-on-your-raspberry-pi/
 
-cd 
-mkdir -p ~/code/opencv
-cd ~/code/opencv
-#git clone
-#git clone
-cd opencv
-mkdir build
-cd build
-
 sudo apt-get install build-essential cmake pkg-config -y
 sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
@@ -18,7 +9,16 @@ sudo apt-get install libxvidcore-dev libx264-dev -y
 sudo apt-get install libgtk2.0-dev libgtk-3-dev -y
 sudo apt-get install libatlas-base-dev gfortran -y
 sudo apt-get install python2.7-dev python3-dev -y
-pip install numpy
+sudo pip install numpy
+
+mkdir -p ~/code/opencv
+cd ~/code/opencv
+git clone https://github.com/opencv/opencv.git
+cd ~/code/opencv
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv
+mkdir build
+cd build
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D CMAKE_INSTALL_PREFIX=/usr/local \
@@ -26,6 +26,6 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D OPENCV_EXTRA_MODULES_PATH=~/code/opencv/opencv_contrib/modules \
       -D BUILD_EXAMPLES=ON ..
 
-make -j4
+make
 sudo make install
 sudo ldconfig
--- jenkins/toinstall.sh
+++ jenkins/toinstall.sh
@@ -7,6 +7,14 @@ sudo apt-get install autopep8 -y
 sudo apt-get install python-examples -y
 sudo apt-get install python-dev libffi-dev libssl-dev -y
 sudo apt-get install python-logilab-common -y
+sudo apt-get install wiringpi -y
+sudo apt-get install git-core -y
+sudo apt-get install libcwiid1 lswm wmgui wminput -y
+sudo apt-get install cloc -y
+sudo apt-get install doxygen -y
+
+sudo apt remove wolfram-engine -y
+
 sudo pip install --upgrade pip
 sudo pip install --upgrade ndg-httpsclient 
 sudo pip install --upgrade setuptools
@@ -15,18 +23,14 @@ sudo pip install yapf
 sudo pip install pytest
 sudo pip install pytest-cov
 sudo pip install colorlog
-sudo apt-get purge wiringpi
-sudo apt-get install git-core
-cd code
-git clone git://git.drogon.net/wiringPi
-cd wiringPi
-git pull origin
-sudo ./build
-cd wiringPi
-make static
-sudo make install-static
-cd ../..
-sudo apt-get install libcwiid1 lswm wmgui wminput
-#sudo pip install tesseract
-#sudo pip install lept
-sudo apt install libgtk2.0-dev
+sudo pip install coverage
+
+#cd code
+#git clone git://git.drogon.net/wiringPi
+#cd wiringPi
+#git pull origin
+#sudo ./build
+#cd wiringPi
+#make static
+#sudo make install-static
+#cd ../..
