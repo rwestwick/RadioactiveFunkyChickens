b32e1573ee808031de97e4156aee7eca930493d9
Matt <matt_lees@yahoo.com>
2018-03-07 00:51:50 +0000

    updated alternate gpiozero library servo test

--- testgpiozero_servo.py
+++ testgpiozero_servo.py
@@ -1,18 +1,60 @@
+from __future__ import division
 import GPIOLayout
+#from gpiozero import Servo
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(GPIOLayout.DUCK_SHOOT_FIRE_GPIO)
 
-servo.min()
-sleep(1)
+#servo = AngularServo(27, min_angle=-90, max_angle=90, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, frame_width=5/1000)
+servo = AngularServo(27, min_angle=-90, max_angle=90)
+#servo = AngularServo(27)
+print "setting to 0"
+#servo.angle = 0
+#print servo.value
+sleep(2)
+print "mid"
 servo.mid()
-sleep(1)
+#print servo.value
+sleep(2)
+print "min"
+servo.min()
+print servo.value
+sleep(2)
+print "mid"
+servo.mid()
+print servo.value
+sleep(2)
+print "max"
 servo.max()
-sleep(1)
-servo.angle(-45)
-sleep(1)
-servo.angle(45)
-sleep(1)
+print servo.value
+sleep(2)
+print "-90"
+servo.angle = -90
+print servo.value
+sleep(2)
+print "90"
+servo.angle = 90
+print servo.value
+sleep(2)
+print "0"
+servo.angle = 0
+print servo.value
+sleep(2)
 servo.detach()
-sleep(1)
+sleep(2)
+
+exit()
+
+servo1 = Servo(27, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, frame_width=20/1000)
+servo1.min()
+print servo.value
+sleep(2)
+servo1.mid()
+print servo.value
+sleep(2)
+servo1.max()
+print servo.value
+sleep(2)
+servo1.detach()
+
+exit()
